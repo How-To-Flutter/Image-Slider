9c62ad3f88ffbfabc084f286238e66e1429a82c0
How To Flutter <109933224+How-To-Flutter@users.noreply.github.com>
2022-07-26 20:08:43 -0300

    Add files via upload

---
+++ slider.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:carousel_slider/carousel_controller.dart';
+
+class SliderP extends StatefulWidget {
+  SliderP({Key? key}) : super(key: key);
+
+  @override
+  State<SliderP> createState() => _SliderPState();
+}
+
+class _SliderPState extends State<SliderP> {
+  int activeIndex = 0;
+  final controller = CarouselController();
+  final urlImages = [
+    'https://images.unsplash.com/photo-1612825173281-9a193378527e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=499&q=80',
+    'https://images.unsplash.com/photo-1580654712603-eb43273aff33?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    'https://images.unsplash.com/photo-1627916607164-7b20241db935?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80',
+    'https://images.unsplash.com/photo-1522037576655-7a93ce0f4d10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    'https://images.unsplash.com/photo-1570829053985-56e661df1ca2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 40, 40, 35),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CarouselSlider.builder(
+                carouselController: controller,
+                itemCount: urlImages.length,
+                itemBuilder: (context, index, realIndex) {
+                  final urlImage = urlImages[index];
+                  return buildImage(urlImage, index);
+                },
+                options: CarouselOptions(
+                    height: 400,
+                    autoPlay: true,
+                    enableInfiniteScroll: false,
+                    autoPlayAnimationDuration: Duration(seconds: 2),
+                    enlargeCenterPage: true,
+                    onPageChanged: (index, reason) =>
+                        setState(() => activeIndex = index))),
+            SizedBox(height: 12),
+            buildIndicator()
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildIndicator() => AnimatedSmoothIndicator(
+        onDotClicked: animateToSlide,
+        effect: ExpandingDotsEffect(dotWidth: 15, activeDotColor: Colors.blue),
+        activeIndex: activeIndex,
+        count: urlImages.length,
+      );
+
+  void animateToSlide(int index) => controller.animateToPage(index);
+}
+
+Widget buildImage(String urlImage, int index) =>
+    Container(child: Image.network(urlImage, fit: BoxFit.cover));
